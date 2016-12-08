8cec1d1b68e216956fb056d9a89d22480d9c60e5
edwardanchundia <edward.anchundia@gmail.com>
2016-12-08 12:50:24 -0500

    Midterm

--- AC3.2-MidtermSwiftFundamentals/ViewController.swift
+++ AC3.2-MidtermSwiftFundamentals/ViewController.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+class C4QStudent {
+    let name: String
+    let id: Int
+    let favoriteLunch: String
+    
+    init(name: String, id: Int, favoriteLunch: String) {
+        self.name = name
+        self.id = id
+        self.favoriteLunch = favoriteLunch
+    }
+}
+
 enum Ship: String {
     case battle
     case friend
@@ -24,6 +36,8 @@ class ViewController: UIViewController {
                         "Arkansas" : "Little Rock",
                         "California" : "Sacramento",
                         "Colorado" : "Denver"]
+    
+    var c4QStudentsArr = [C4QStudent]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,14 +57,14 @@ class ViewController: UIViewController {
         print("A. 1")
         // replace this comment with your answer
         // if you do the extra credit, keep it above the line
-        print("---------")
+        print(sumOfArrInt(arr: someInts))
         
         // Q. 2
         // You're back in 3rd grade. What's the capital of Arkansas?
         // Print the answer using the someCapitals dictionary.
         print("\nA. 2")
         // replace this comment with your answer
-        print("---------")
+        print(someCapitals["Arkansas"]!)
         
         // Q. 3
         // You're in Denver for legal recreational reasons and have subsequently
@@ -58,7 +72,7 @@ class ViewController: UIViewController {
         // to find out and print the state.
         print("\nA. 3")
         // replace this comment with your answer
-        print("---------")
+        print(findStateForDenver())
         
         // Q. 4
         // Write a function that takes no arguments and returns
@@ -66,7 +80,7 @@ class ViewController: UIViewController {
         // Ship enum.
         print("\nA. 4")
         // replace this comment with your answer
-        print("---------")
+        print(shipString())
 
         // Q. 5
         // Write a function that takes an instance of Ship as its parameter
@@ -74,15 +88,16 @@ class ViewController: UIViewController {
         // Test and print both cases
         print("\nA. 5 test 1")
         // replace this comment with your answer
+        print(shipSafeOrDanger(rawValue: Ship.battle.rawValue))
         
         print("\nA. 5 test 2")
         // replace this comment with your answer
-        print("---------")
+        print(shipSafeOrDanger(rawValue: Ship.friend.rawValue))
 
         // Q. 6
         // The year is 2018. You've been called on to decipher a list of inputs that 
         // are suspected to represent numbers. Apple, in an effort to do its part in
-        // keeping jobs in America has removed the map function from the library.
+        // keeping jobs in America has removed the filter function from the library.
         // Write a simple clone of the map function. It should take as its two
         // parameters an array of String and a closure of type (String)->Int?
         // Call your function with a suitable closure to interpret the elements
@@ -94,7 +109,7 @@ class ViewController: UIViewController {
         print("\nA. 6")
         // replace this comment with your answer
         // if you do the extra credit, keep it above the line
-        print("---------")
+        print("----------")
         
         // Q. 7
         // You got a job translating loan words from English into French. For some unknown reason
@@ -103,7 +118,7 @@ class ViewController: UIViewController {
         // Output: ["canoeing", "hiking", "camping", "shampooing"]
         print("\nA. 7")
         // replace this comment with your answer
-        print("---------")
+        print(wordsWithING(arr: mixedVerbs))
         
         // Q. 8a
         // Create a class C4QStudent with the following properties:
@@ -128,13 +143,15 @@ class ViewController: UIViewController {
         
         print("\nA. 8b")
         // replace this comment with your answer
-        print("---------")
+        c4QStudents()
+        print(dump(c4QStudentsArr))
 
         // Q. 8c
         // Create a new array by sorting by name. dump() it.
         print("\nA. 8c")
         // replace this comment with your answer
-        print("---------")
+//        sortByName()
+//        print(dump(newNameSortedArr))
         
         // Q. 8d
         // Create a new array by sorting by id. dump() it.
@@ -161,7 +178,7 @@ class ViewController: UIViewController {
         //     "You can append lines like this if you " +
         //     "want to be verbose."
         // )
-        print("---------")
+        print("Since we are using a class, which is passed by refrence instead of value, all arrays will look alike. Difference of structs and classes.")
         
         // Q. 10.
         // How would it differ if C4QStudent was a struct?
@@ -172,9 +189,87 @@ class ViewController: UIViewController {
         //     "You can append lines like this if you " +
         //     "want to be verbose."
         // )
-        print("---------")
+        print("Since structs are passed by value, they will have different arrays.")
         
     }
+    
+    func sumOfArrInt(arr: [Int]) -> Int {
+        var sum = 0
+        for element in arr {
+            sum += element
+        }
+        return sum
+    }
+    
+    func findStateForDenver() {
+        for element in someCapitals.keys {
+            if someCapitals[element] == "Denver" {
+                print(element)
+            }
+        }
+    }
+    
+    func shipString() -> String {
+
+        return "Hi, friend, let's do battle"
+    }
+    
+    func shipSafeOrDanger(rawValue: Ship.RawValue) -> String {
+        switch rawValue {
+        case "battle":
+            return "DANGER"
+        case "friend":
+            return "SAFE"
+        default:
+            return "None"
+        }
+    }
+    
+//    func replaceFilterFunc(arr: [String], closure: (String) -> Int?) -> [Int] {
+//        let newArr = [Int]()
+//        let myClosure =
+//        
+//        return newArr
+//    }
+    
+    func wordsWithING(arr: [String]) -> [String] {
+        var wordsWithING = [String]()
+        
+        for words in arr {
+            if words.hasSuffix("ing") {
+                wordsWithING.append(words)
+            }
+        }
+        
+        return wordsWithING
+        
+    }
+
+    func c4QStudents() {
+        let jermaine = C4QStudent(name: "Jermaine", id: 83, favoriteLunch: "Millie's")
+        let kadell = C4QStudent(name: "Kadell", id: 23, favoriteLunch: "Vernon Blvd. Chinese")
+        let miti = C4QStudent(name: "Miti", id: 77, favoriteLunch: "Subway")
+        let sabrina = C4QStudent(name: "Sabrina", id: 68, favoriteLunch: "Vernon Blvd. Pizza")
+        let marcel = C4QStudent(name: "Marcel", id: 39, favoriteLunch: "Court Sq. Diner")
+        
+        c4QStudentsArr.append(jermaine)
+        c4QStudentsArr.append(kadell)
+        c4QStudentsArr.append(miti)
+        c4QStudentsArr.append(sabrina)
+        c4QStudentsArr.append(marcel)
+    }
+    
+//    var newNameSortedArr = [Any]()
+//    func sortByName() {
+//        for value in 0..<c4QStudentsArr.count {
+//            while value != 4 {
+//                if c4QStudentsArr[value].name > c4QStudentsArr[value + 1] {
+//                    newNameSortedArr.append(<#T##newElement: Element##Element#>)
+//                }
+//            }
+//        }
+//    }
+
 }
 
 
