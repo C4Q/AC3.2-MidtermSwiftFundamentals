f2a9940e18763b4a0abea90daf3681a65fca8e93
Marcel324 <mchaucer324@gmail.com>
2016-12-08 12:50:47 -0500

    Fundamentals

--- AC3.2-MidtermSwiftFundamentals/ViewController.swift
+++ AC3.2-MidtermSwiftFundamentals/ViewController.swift
@@ -41,23 +41,29 @@ class ViewController: UIViewController {
         // E.C. Solve using a higher order function.
         
         print("A. 1")
-        // replace this comment with your answer
-        // if you do the extra credit, keep it above the line
-        print("---------")
+        
+        //print(addUpArray(arr: someInts))
+        print(addUpArrayWithHigherOrderFunc(arr: someInts))
+        print("------------")
         
         // Q. 2
         // You're back in 3rd grade. What's the capital of Arkansas?
         // Print the answer using the someCapitals dictionary.
         print("\nA. 2")
-        // replace this comment with your answer
-        print("---------")
+        print(someCapitals["Arkansas"]!)
+        print("----------")
         
         // Q. 3
         // You're in Denver for legal recreational reasons and have subsequently
         // forgotten what state you're in. Use the someCapitals dictionary
         // to find out and print the state.
         print("\nA. 3")
-        // replace this comment with your answer
+        for states in someCapitals {
+            if states.value == "Denver" {
+                print(states.key)
+            }
+        }
+
         print("---------")
         
         // Q. 4
@@ -65,7 +71,7 @@ class ViewController: UIViewController {
         // the String, "Hi, friend, let's do battle" using the
         // Ship enum.
         print("\nA. 4")
-        // replace this comment with your answer
+        print(printThisString())
         print("---------")
 
         // Q. 5
@@ -73,10 +79,10 @@ class ViewController: UIViewController {
         // and returns "SAFE" if it's .friend, or "DANGER" if it's .battle.
         // Test and print both cases
         print("\nA. 5 test 1")
-        // replace this comment with your answer
+        print(safeOrDanger(otherShip: .friend))
         
         print("\nA. 5 test 2")
-        // replace this comment with your answer
+        print(safeOrDanger(otherShip: .battle))
         print("---------")
 
         // Q. 6
@@ -92,7 +98,8 @@ class ViewController: UIViewController {
         // returns more numbers. I was able to get 4 Ints.
         
         print("\nA. 6")
-        // replace this comment with your answer
+        //Closures still trip me up.
+        //print(arrayAndClosure(arr: numb3rs))
         // if you do the extra credit, keep it above the line
         print("---------")
         
@@ -102,16 +109,23 @@ class ViewController: UIViewController {
         // and filter them so only those in the -ing form remain. Use the mixedVerbs property as input.
         // Output: ["canoeing", "hiking", "camping", "shampooing"]
         print("\nA. 7")
-        // replace this comment with your answer
+        print(containsING(arr: mixedVerbs))
         print("---------")
         
         // Q. 8a
         // Create a class C4QStudent with the following properties:
-        //   
-        //   let name: String
-        //   let id: Int
-        //   var favoriteLunch: String
-        // 
+        class C4QStudent {
+           let name: String
+           let id: Int
+           var favoriteLunch: String
+            
+            init(name: String, id: Int, favoriteLunch: String) {
+                self.name = name
+                self.id = id
+                self.favoriteLunch = favoriteLunch
+            }
+        }
+        
         // Create an init method that sets all three properties from parameters passed in.
         
         // Q. 8b
@@ -127,19 +141,27 @@ class ViewController: UIViewController {
         //
         
         print("\nA. 8b")
-        // replace this comment with your answer
+        let Jermaine = C4QStudent.init(name: "Jermaine", id: 83, favoriteLunch: "Millie's")
+        let Kadell = C4QStudent(name: "Kadell", id: 23, favoriteLunch: "Vernon Blvd. Chinese")
+        let Miti = C4QStudent.init(name: "Miti", id: 77, favoriteLunch: "Subway")
+        let Sabrina = C4QStudent.init(name: "Sabrina", id: 68, favoriteLunch: "Vernon Blvd. Pizza")
+        let Marcel = C4QStudent.init(name: "Marcel", id: 39, favoriteLunch: "Court Sq. Diner")
+        let studentArray = [Marcel, Kadell, Miti, Sabrina, Marcel]
+        dump(studentArray)
         print("---------")
 
         // Q. 8c
         // Create a new array by sorting by name. dump() it.
         print("\nA. 8c")
-        // replace this comment with your answer
+        let sortedNameArray = [Jermaine, Kadell, Miti, Sabrina, Marcel].map{$0.name}.sorted {$0 < $1}
+        dump(sortedNameArray)
         print("---------")
         
         // Q. 8d
         // Create a new array by sorting by id. dump() it.
         print("\nA. 8d")
-        // replace this comment with your answer
+        let sortedIdArray = [Jermaine, Kadell, Miti, Sabrina, Marcel].map{$0.id}.sorted {$0 < $1 }
+        dump(sortedIdArray)
         print("---------")
 
         // Q. 8e
@@ -147,7 +169,8 @@ class ViewController: UIViewController {
         //
         // E.C. Solve using a higher order function.
         print("\nA. 8e")
-        // replace this comment with your answer
+        let tamashiiRamenLunch = [Jermaine, Kadell, Miti, Sabrina, Marcel].map{$0.name.replacingOccurrences(of: $0.name, with: "Tamashii Ramen")}
+        dump(tamashiiRamenLunch)
         // if you do the extra credit, keep it above the line
         print("---------")
         
@@ -157,10 +180,9 @@ class ViewController: UIViewController {
         // Print the answer here
         print("\nA. 9")
         
-        // print(
-        //     "You can append lines like this if you " +
-        //     "want to be verbose."
-        // )
+         print(
+        "If multiple arrays refer to the same object then the changes would be seen in all of the arrays"
+        )
         print("---------")
         
         // Q. 10.
@@ -168,13 +190,59 @@ class ViewController: UIViewController {
         // Print the answer here
         print("\nA. 10")
 
-        // print(
-        //     "You can append lines like this if you " +
-        //     "want to be verbose."
-        // )
+         print(
+             "For one, we would get a free initializer so the  " +
+             "writing it out would be a bit different and shorter, also a struct"
+         )
         print("---------")
         
     }
+    func addUpArray(arr: [Int]) -> Int {
+        var sum = 0
+        for i in arr {
+            sum += i
+        }
+        return sum
+    }
+    
+    func addUpArrayWithHigherOrderFunc(arr: [Int]) -> Int {
+        let addTwo: (Int, Int) -> Int = { x, y in x + y }
+        return arr.reduce(0, addTwo)
+    }
+
+    func printThisString() -> String {
+        return "Hi, \(Ship.friend), let's do \(Ship.battle)"
+    }
+    
+    func safeOrDanger(otherShip: Ship) -> String {
+        if otherShip == .friend {
+            return "SAFE"
+        } else {
+            return "DANGER"
+        }
+    }
+    
+    func containsING(arr: [String]) -> [String] {
+        var finalArray: [String] = []
+        for word in arr {
+            if word.contains("ing") {
+                finalArray.append(word)
+            }
+        }
+        return finalArray
+    }
+    
+    func arrayAndClosure(arr: [String], closure: (String) -> Int?) -> [Int] {
+        var finalArray: [Int] = []
+        for i in arr {
+            if (Int(i) != nil) {
+                finalArray.append(Int(i)!)
+            }
+        }
+        return finalArray
+    }
+
+
 }
 
 
