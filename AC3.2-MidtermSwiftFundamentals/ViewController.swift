787fbd0774edd8fe04b7c81fbb5cf1aff4c5b600
Tyler Newton <tylernewton@C4Qs-MacBook-Pro-38.local>
2016-12-08 12:57:01 -0500

    part 1 complete.

--- AC3.2-MidtermSwiftFundamentals/ViewController.swift
+++ AC3.2-MidtermSwiftFundamentals/ViewController.swift
@@ -43,14 +43,14 @@ class ViewController: UIViewController {
         print("A. 1")
         // replace this comment with your answer
         // if you do the extra credit, keep it above the line
-        print("---------")
+        print(answer1(somearray: someInts))
         
         // Q. 2
         // You're back in 3rd grade. What's the capital of Arkansas?
         // Print the answer using the someCapitals dictionary.
         print("\nA. 2")
         // replace this comment with your answer
-        print("---------")
+        print(answer2(someDict: someCapitals))
         
         // Q. 3
         // You're in Denver for legal recreational reasons and have subsequently
@@ -58,7 +58,7 @@ class ViewController: UIViewController {
         // to find out and print the state.
         print("\nA. 3")
         // replace this comment with your answer
-        print("---------")
+        print(answer3(location: mylocation))
         
         // Q. 4
         // Write a function that takes no arguments and returns
@@ -66,7 +66,7 @@ class ViewController: UIViewController {
         // Ship enum.
         print("\nA. 4")
         // replace this comment with your answer
-        print("---------")
+        print(answer4())
 
         // Q. 5
         // Write a function that takes an instance of Ship as its parameter
@@ -74,10 +74,11 @@ class ViewController: UIViewController {
         // Test and print both cases
         print("\nA. 5 test 1")
         // replace this comment with your answer
+        print(answerFive1(instance: Ship.friend))
         
         print("\nA. 5 test 2")
         // replace this comment with your answer
-        print("---------")
+        print(answerFive2(instance: Ship.battle))
 
         // Q. 6
         // The year is 2018. You've been called on to decipher a list of inputs that 
@@ -103,7 +104,7 @@ class ViewController: UIViewController {
         // Output: ["canoeing", "hiking", "camping", "shampooing"]
         print("\nA. 7")
         // replace this comment with your answer
-        print("---------")
+        print(answer7(verbs: mixedVerbs))
         
         // Q. 8a
         // Create a class C4QStudent with the following properties:
@@ -174,6 +175,83 @@ class ViewController: UIViewController {
         // )
         print("---------")
         
+    }
+    func answer1(somearray: [Int]) -> Int {
+        var sum = 0
+        for number in somearray {
+            if number != sum {
+                sum += number
+            }
+        }
+        return sum
+    }
+    
+    func answer2(someDict: [String:String]) -> String {
+        var stateCapital = ""
+        for (state, capital) in someDict {
+            if state == "Arkansas"{
+                stateCapital = ("\(state), \(capital)")
+            }
+        }
+        return stateCapital
+    }
+    
+    let mylocation = "Denver"
+    
+    func answer3(location: String) -> String {
+        for (state, capital) in someCapitals {
+            if capital == mylocation {
+                print("You are in \(capital), \(state) bro!")
+            }
+            else {
+                continue
+            }
+        }
+        return "Ok cool, I am in \(mylocation)."
+    }
+    func answer4() -> String {
+        return "Hi, \(Ship.friend), let's do \(Ship.battle)"
+    }
+    func answerFive1(instance: Ship) -> String {
+        let friend = Ship.friend
+        var safe = ""
+        
+        if friend == Ship.friend{
+            safe += "SAFE"
+        }
+        return safe
+    }
+    func answerFive2(instance: Ship) -> String {
+        let foe = Ship.battle
+        var danger = ""
+        
+        if foe == Ship.battle{
+            danger += "DANGER"
+        }
+        return danger
+    }
+    func answer7(verbs: [String]) -> [String] {
+        var frenchVerb: [String] = []
+        
+        for verb in verbs{
+            if verb.contains("ing") {
+                frenchVerb.append(verb)
+            }
+        }
+        return frenchVerb
+    }
+    
+    // I didn't have time to call the method that would set the values for this class but I was planning on creating something like var students: [C4QStudent]() and then append values into it by either parsing data or taking values out of some type of dictionary.
+    class C4QStudent {
+        let name: String
+        let id: Int
+        var favoriteLunch: String
+        
+        init(name: String, id: Int, favoriteLunch: String) {
+            self.name = name
+            self.id = id
+            self.favoriteLunch = favoriteLunch
+        }
     }
 }
 
